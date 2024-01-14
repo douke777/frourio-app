612b8e56d676d782f1503a1f3c28cfbea7f28388
dtom777 <ethan230204@gmail.com>
2024-01-15 08:52:39 +0900

    add: tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react';
 
 import api from '@/lib/apiClient';
-import { Header } from '@/components/Header';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [greeting, setGreeting] = useState('loading...');
 
   useEffect(() => {
@@ -16,18 +14,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <p>{greeting}</p>
     </>
   );
