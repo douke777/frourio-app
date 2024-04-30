0e6c8c43bb352be80ddda87c1be8293b3394107f
dtom777 <ethan230204@gmail.com>
2024-04-30 21:21:17 +0900

    delete: 不要な関数を削除

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,11 +7,3 @@ export const convert = ({ ref, ...others }: UseFormRegisterReturn) => {
 export const resolve = (field?: { message?: string }) => {
   return field?.message ? [field?.message] : undefined;
 };
-
-export const getAsString = (value: string | string[]): string => {
-  if (Array.isArray(value)) {
-    return value[0];
-  }
-
-  return value;
-};
