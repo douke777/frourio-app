284c0f5df06df0be09c7a056cda7923250cebcd6
dtom777 <ethan230204@gmail.com>
2024-05-14 08:41:30 +0900

    update: baseURL

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 import api from '$/api/$api';
 
+const baseURL = import.meta.env.PROD ? '/api' : 'http://localhost:8888/api';
+
 axios.defaults.withCredentials = true;
-export const apiClient = api(aspida(axios, { baseURL: 'http://localhost:8888/api' }));
+export const apiClient = api(aspida(axios, { baseURL }));
 
 export default apiClient;
